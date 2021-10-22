4d0f60c7c48f6ad500cf163e1ac9cf6905d7a40a
RedyPutraSembada <92964495+RedyPutraSembada@users.noreply.github.com>
2021-10-22 21:31:11 +0700

    Aplikasi List

---
+++ app/src/main/java/com/example/listviewapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.listviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lvItem;
+
+    private String[] namaNegara = new String[]{
+            "Indonesia", "Malaysia", "singapore",
+            "italia", "Inggris", "Belanda",
+            "Argentina", "Chile",
+            "Mesir", "Uganda"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("List Sederhana");
+
+        lvItem = findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, namaNegara);
+
+        lvItem.setAdapter(adapter);
+
+        lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Memilih : "+namaNegara[position], Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
